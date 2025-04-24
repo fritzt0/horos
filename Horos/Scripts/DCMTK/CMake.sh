be9224d1257c6512217130600cf001f6dba4c256
Felix Ritter <13329877+fritzt0@users.noreply.github.com>
2025-04-24 15:10:27 +0200

    apply fixes to DCMTK sources

--- Horos/Scripts/DCMTK/CMake.sh
+++ Horos/Scripts/DCMTK/CMake.sh
@@ -28,6 +28,8 @@ fi
 command -v cmake >/dev/null 2>&1 || { echo >&2 "error: building $TARGET_NAME requires CMake. Please install CMake. Aborting."; exit 1; }
 command -v pkg-config >/dev/null 2>&1 || { echo >&2 "error: building $TARGET_NAME requires pkg-config. Please install pkg-config. Aborting."; exit 1; }
 
+patch --directory "$SRCROOT/DCMTK" --forward -p1 --input "$SRCROOT/Horos/Scripts/DCMTK/compile-fixes.patch" || true
+
 mv "$cmake_dir" "$cmake_dir.tmp"
 [ -d "$install_dir" ] && mv "$install_dir" "$install_dir.tmp"
 rm -Rf "$cmake_dir.tmp" "$install_dir.tmp"
